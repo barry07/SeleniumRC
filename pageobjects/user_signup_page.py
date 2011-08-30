fa8ededd7e191dedde6697741eb4e165c06a1a60
Barry <horgan.barry@gmail.com>
2011-08-30 22:56:33 +1000

    added

---
+++ pageobjects/user_signup_page.py
@@ -0,0 +1,15 @@
+import re
+
+class UserSignupPage
+
+	_page_title = "User Signup | Freelancer.com.au"
+
+	#Input boxes
+	_new_username_locator = "//input[@id='tbx_username']"
+	_email_address_locator = "//input[@id='tbx_email']"
+	_password_locator = "//input[@id='passwd']"
+	_repeat_password_locator = "//input[@id='passwd1']"
+
+	#Button
+	_join_button_locator = "//a[@id='submit']"
+
